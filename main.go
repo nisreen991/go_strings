c9b712eba0284bf4a627daf4584f6120c40aee46
nisreen991 <50657375+nisreen991@users.noreply.github.com>
2025-04-16 22:52:16 +0530

    some more functions

--- main.go
+++ main.go
@@ -7,13 +7,20 @@ import (
 
 func main() {
 
-	s := "Hello World"
+	s := "Hello World "
+	s1 := "India-is-a-great-country"
 	substr := "World"
 	fmt.Println(containsSubstring(s, substr))
 	fmt.Println(upperCase(s))
 	fmt.Println(lowerCase(s))
 	fmt.Println(index(s, substr))
 	fmt.Println(length(s))
+	fmt.Println(split(s1))
+	fmt.Print(repeat(s))
+	fmt.Println(replace(s))
+	fmt.Println(join(split(s1)))
+	fmt.Println(hasPrefix(s))
+	fmt.Println(hasSuffix(s))
 }
 
 func containsSubstring(s string, substr string) bool {
@@ -34,4 +41,28 @@ func index(s string, substr string) int {
 
 func length(s string) int {
 	return len(s)
+}
+
+func split(s string) []string {
+	return strings.Split(s, "-")
+}
+
+func repeat(s string) string {
+	return strings.Repeat(s, 4)
+}
+
+func replace(s string) string {
+	return strings.Replace(s, "W", "w", 4) // learn more about this
+}
+
+func join(s []string) string {
+	return strings.Join(s, "-")
+}
+
+func hasPrefix(s string) bool {
+	return strings.HasPrefix(s, "He")
+}
+
+func hasSuffix(s string) bool {
+	return strings.HasSuffix(s, "rld")
 }
